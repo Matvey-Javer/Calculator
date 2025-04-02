5c979277ec0af614c4091f601ed4f0dc51fb2bcb
Matvey Shavlov <m.shavloff@gmail.com>
2025-04-02 10:18:10 +0300

    replaced arrayList with CopyOnWriteArrayList

--- src/Test.java
+++ src/Test.java
@@ -1,49 +1,54 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Test {
+    private static Scanner scanner1 = new Scanner(System.in);
+    private static Scanner scanner2 = new Scanner(System.in);
+    private static Scanner scanner3 = new Scanner(System.in);
+    private static Scanner scanner4 = new Scanner(System.in);
+
     private static String firstValue;
     private static String firstSymbol;
     private static String secondValue;
     private static String secondSymbol;
 
 
-    private static List<String> firstValues = new ArrayList<>();
-    private static List<String> firstSymbols = new ArrayList<>();
-    private static List<String> secondValues = new ArrayList<>();
-    private static List<String> secondSymbols = new ArrayList<>();
+    private static List<String> firstValues = new CopyOnWriteArrayList<>();
+    private static List<String> firstSymbols = new CopyOnWriteArrayList<>();
+    private static List<String> secondValues = new CopyOnWriteArrayList<>();
+    private static List<String> secondSymbols = new CopyOnWriteArrayList<>();
 
 
     public static void main(String[] args) throws InterruptedException {
-        Scanner scanner = new Scanner(System.in);
-
-        while (!scanner.nextLine().equals("=")) {
+        while ((!scanner1.nextLine().equals("=")) | (!scanner2.nextLine().equals("="))
+                | (!scanner3.nextLine().equals("=")) | (!scanner4.nextLine().equals("="))) {
 
             Thread thread1 = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    AddElementsInLists.addingForFirstList(firstValue, scanner, firstValues);
+                    AddElementsInLists.addingForFirstList(firstValue, scanner1, firstValues);
                 }
             });
 
             Thread thread2 = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    AddElementsInLists.addingForFirstList(firstSymbol, scanner, firstSymbols);
+                    AddElementsInLists.addingForFirstList(firstSymbol, scanner2, firstSymbols);
                 }
             });
             Thread thread3 = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    AddElementsInLists.addingForFirstList(secondValue, scanner, secondValues);
+                    AddElementsInLists.addingForFirstList(secondValue, scanner3, secondValues);
                 }
             });
 
             Thread thread4 = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    AddElementsInLists.addingForFirstList(secondSymbol, scanner, secondSymbols);
+                    AddElementsInLists.addingForFirstList(secondSymbol, scanner4, secondSymbols);
                 }
             });
 
