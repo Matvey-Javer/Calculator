e59d607fa2b7fdceaced02b33160edd89f102f3f
Matvey Shavlov <m.shavloff@gmail.com>
2025-04-02 20:24:51 +0300

    predFinal version class Test, but output not wrong

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Test {
     private static Scanner scanner1 = new Scanner(System.in);
@@ -14,54 +13,56 @@ public class Test {
     private static String secondValue;
     private static String secondSymbol;
 
+    private static List<String> firstValues = new ArrayList<>();
+    private static List<String> firstSymbols = new ArrayList<>();
+    private static List<String> secondValues = new ArrayList<>();
+    private static List<String> secondSymbols = new ArrayList<>();
 
-    private static List<String> firstValues = new CopyOnWriteArrayList<>();
-    private static List<String> firstSymbols = new CopyOnWriteArrayList<>();
-    private static List<String> secondValues = new CopyOnWriteArrayList<>();
-    private static List<String> secondSymbols = new CopyOnWriteArrayList<>();
 
+    public static void main(String[] args) throws InterruptedException, NullPointerException {
+        if (firstValue != null | firstSymbol != null | secondValue != null | secondSymbol != null) {
+            while (!firstValue.equals("=") | !secondValue.equals("=") | !secondSymbol.equals("=") | !firstSymbol.equals("=")) {
+                // покумекай над условием выхода из бесконечнонр цикла
 
-    public static void main(String[] args) throws InterruptedException {
-        while ((!scanner1.nextLine().equals("=")) | (!scanner2.nextLine().equals("="))
-                | (!scanner3.nextLine().equals("=")) | (!scanner4.nextLine().equals("="))) {
+                Thread thread1 = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        AddElementsInLists.addingForFirstList(firstValue, scanner1, firstValues);
+                    }
+                });
 
-            Thread thread1 = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    AddElementsInLists.addingForFirstList(firstValue, scanner1, firstValues);
-                }
-            });
+                Thread thread2 = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        AddElementsInLists.addingForSymbolsList1(firstSymbol, scanner2, firstSymbols);
+                    }
+                });
+                Thread thread3 = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        AddElementsInLists.addingForSecondList(secondValue, scanner3, secondValues);
+                    }
+                });
 
-            Thread thread2 = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    AddElementsInLists.addingForFirstList(firstSymbol, scanner2, firstSymbols);
-                }
-            });
-            Thread thread3 = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    AddElementsInLists.addingForFirstList(secondValue, scanner3, secondValues);
-                }
-            });
+                Thread thread4 = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        AddElementsInLists.addingForSymbolsList2(secondSymbol, scanner4, secondSymbols);
+                    }
+                });
 
-            Thread thread4 = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    AddElementsInLists.addingForFirstList(secondSymbol, scanner4, secondSymbols);
-                }
-            });
+                thread1.start();
+                thread2.start();
+                thread3.start();
+                thread4.start();
 
-            thread1.start();
-            thread2.start();
-            thread3.start();
-            thread4.start();
-
-            thread1.join();
-            thread4.join();
-            thread2.join();
-            thread3.join();
+                thread1.join();
+                thread4.join();
+                thread2.join();
+                thread3.join();
+            }
         }
+        //  }
         System.out.println(firstValues);
         System.out.println(firstSymbols);
         System.out.println(secondValues);
