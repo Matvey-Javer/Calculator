ae4b1c6510e8bd2903b3f556c6c57c02dea08e71
Matvey Shavlov <m.shavloff@gmail.com>
2025-04-04 17:17:42 +0300

    started writing a class from the very beginning and add 4 methods: addition, subtraction, multiplication, division

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,32 +1,17 @@
-import java.util.*;
+public class Calculate{
+    public static void addition(){              // +
 
-public class Calculate {
-    private static boolean stopper = true;
-    private static double result;
-    private static List<Double> firstValues = new ArrayList<>();
-    private static List<Double> secondValues = new ArrayList<>();
-    private static List<String> symbolList = new ArrayList<>();
-
-    public static double workOperation(double value, String symbol) {
-        switch (symbol) {
-            case "+" -> result = result + value;
-            case "-" -> result = result - value;
-            case "*" -> result = result * value;
-            case "/" -> result = result / value;
-        }
-        return result;
     }
 
+    public static void subtraction(){           // -
 
-    public static List<String> sortingValues(List<String> values, double value, String symbol) {
-        List<String> symbols = new ArrayList<>();
-        for (int i = 0; i < values.size(); i++) {
-            if (i % 3 == 0) {
-                value = Double.parseDouble(values.get(i)); // здесь может возникнуть рядь исколючений
-            } else {
-                symbol = values.get(i);
-                symbols.add(symbol);
-            }
-        }return symbols;
+    }
+
+    public static void multiplication(){        // *
+
+    }
+
+    public static void division(){              // /
+
     }
 }
