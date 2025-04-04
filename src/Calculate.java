129fe635954d968ea945a2c0ea96b225080a2c2a
Matvey Shavlov <m.shavloff@gmail.com>
2025-04-04 18:16:46 +0300

    add method convertor

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Calculate{
+    public List<Double> converter(List<String> list){
+         List<Double> doubleList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            doubleList.add(Double.parseDouble(list.get(i)));
+        }
+        return doubleList; // Здесь потенциально можно переписать через лямбду
+    }
+
     public static void addition(){              // +
 
     }
