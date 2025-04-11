ebcd050d91a9e050d0b8502e485ade55601bb775
Matvey Shavlov <m.shavloff@gmail.com>
2025-04-11 14:01:04 +0300

    some changes in Calculate class

--- src/Calculate.java
+++ src/Calculate.java
@@ -2,55 +2,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculate {
+
     public static List<Double> converter(List<String> list) {
         List<Double> doubleList = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            doubleList.add(Double.parseDouble(list.get(i)));
+        for (String s : list) {
+            doubleList.add(Double.parseDouble(s));
         }
-        return doubleList; // Здесь потенциально можно переписать через лямбду
+        return doubleList;
     }
 
-    public static void addition() {              // +
-
+    public static Double addition(Double a, Double b) {
+        return a + b;
     }
 
-    public static void subtraction() {           // -
-
+    public static Double subtraction(Double a, Double b) {
+        return a - b;
     }
 
-    public static void multiplication() {        // *
-
+    public static Double multiplication(Double a, Double b) {
+        return a * b;
     }
 
-    public static void division() {              // /
-
+    public static Double division(Double a, Double b) {
+        return a / b;
     }
 
-    public static Double operation(List<Double> doubles, List<String> symbols, Double result) {
-        for (int i = 1; i < doubles.size(); i++) {
-            for (int j = 0; j < symbols.size(); j++) {
-                if (symbols.get(j).equals("+")) {
-                    result = doubles.get(i-1)+ doubles.get(i);
-                    break;
-                } else if (symbols.get(j).equals("-")) {
-                    result = doubles.get(i - 1) - doubles.get(i);
-                    break;
-                }
-            }
-        }
-        return result;
-    }
-
-    public static Double operation2(List<Double> doubles, List<String> symbols, Double result){
-        for (int i = 0; i < doubles.size(); i++) {
-            Double a = doubles.get(i);
-            for (int j = 0; j < symbols.size(); j++) {
-                String s = symbols.get(j);
-                if (s.equals("+")){
-                    result = result + a;
-                } else if (s.equals("-")) {
-                    result = result - a;
-                }
+    public static Double operation(List<Double> doubles, List<String> symbols) {
+        Double result = 0.0;
+        for (int j = 1; j < doubles.size(); j++) {
+            switch (symbols.get(j - 1)) {
+                case "+" -> result = addition(doubles.get(j - 1), doubles.get(j));
+                case "-" -> result = subtraction(doubles.get(j - 1), doubles.get(j));
+                case "*" -> result = multiplication(doubles.get(j - 1), doubles.get(j));
+                case "/" -> result = division(doubles.get(j - 1), doubles.get(j));
             }
         }
         return result;
@@ -68,9 +52,6 @@ public class Calculate {
         symbols.add("+");
         symbols.add("+");
         symbols.add("-");
-        System.out.println(operation2(doubles, symbols, result));
-        // System.out.println(result);
+        System.out.println(operation(doubles, symbols, result));
     }
-
-    // подумай, а что если использовать 2 листа а не 3, там действия совершать легче чем с 3????????
 }
