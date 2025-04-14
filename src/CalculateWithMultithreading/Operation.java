773e1d08bb2b9068e7a7577df62c496a15d53da0
Matvey Shavlov <m.shavloff@gmail.com>
2025-04-14 17:57:02 +0300

    predFinal version classes Main, Operation and Calculate with multithreadings

--- src/CalculateWithMultithreading/Operation.java
+++ src/CalculateWithMultithreading/Operation.java
@@ -1,4 +1,30 @@
 package CalculateWithMultithreading;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Operation {
+    public static List<Double> converter(List<String> list) {
+        List<Double> doubleList = new ArrayList<>();
+        for (String s : list) {
+            doubleList.add(Double.parseDouble(s));
+        }
+        return doubleList;
+    }
+
+    public static Double addition(Double a, Double b) {
+        return a + b;
+    }
+
+    public static Double subtraction(Double a, Double b) {
+        return a - b;
+    }
+
+    public static Double multiplication(Double a, Double b) {
+        return a * b;
+    }
+
+    public static Double division(Double a, Double b) {
+        return a / b;
+    }
 }
